c12646d370df2bc83208ee7c96c283f81ff55774
Michael Spieler <michael.spieler@epfl.ch>
2016-03-11 13:41:08 +0100

    Add Makefile, main.c and config files

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <ch.h>
+#include <hal.h>
+
+static THD_WORKING_AREA(waThread1, 128);
+static THD_FUNCTION(Thread1, arg)
+{
+    (void)arg;
+    chRegSetThreadName("blinker");
+    while (true) {
+        palSetPad(GPIOD, GPIOD_LED3);
+        chThdSleepMilliseconds(500);
+        palClearPad(GPIOD, GPIOD_LED3);
+        chThdSleepMilliseconds(500);
+    }
+}
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+
+    sdStart(&SD2, NULL);
+    palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
+    palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
+
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+    while (true) {
+        chThdSleepMilliseconds(500);
+    }
+}
